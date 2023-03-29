c8314f59a66ddfab583e393e411efa6e10ca5345
Katja Danilova <katja.danilova@protonmail.com>
2023-03-29 17:21:06 +0300

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -101,18 +101,6 @@ val generateApiClient = tasks.register("generateApiClient",GenerateTask::class){
     this.configOptions.put("serializationLibrary", "jackson")
 }
 
-coverallsJacoco {
-    reportPath = "build/jacoco-report/jacoco.xml"
-}
-
-tasks.test {
-    finalizedBy(tasks.jacocoTestReport)
-    configure<JacocoTaskExtension> {
-        excludeClassLoaders = listOf("*QuarkusClassLoader*")
-        setDestinationFile(layout.buildDirectory.file("jacoco-quarkus.exec").get().asFile)
-    }
-}
-
 tasks.named("compileKotlin") {
     dependsOn(generateApiSpec)
 }
