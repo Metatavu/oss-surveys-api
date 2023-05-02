529bc79597353a0bad8b4db26bf04dce3876309f
Ville Juutila <ville.juutila@gmail.com>
2023-05-02 12:29:46 +0300

    Added missing native tests

---
+++ src/native-test/kotlin/fi/metatavu/oss/api/test/functional/tests/NativeDeviceSurveysTestIT.kt
@@ -0,0 +1,10 @@
+package fi.metatavu.oss.api.test.functional.tests
+
+import fi.metatavu.oss.api.test.functional.resources.LocalTestProfile
+import io.quarkus.test.junit.QuarkusIntegrationTest
+import io.quarkus.test.junit.TestProfile
+
+@QuarkusIntegrationTest
+@TestProfile(LocalTestProfile::class)
+class NativeDeviceSurveysTestIT: DeviceSurveysTestIT() {
+}
